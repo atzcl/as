43cf2a0d4d919f2d464934412a69c82dd5e8b3e3
植成樑 <atzcl0310@gmail.com>
2022-05-02 23:16:05 +0800

    update

---
+++ utils/axios.js
@@ -0,0 +1,28 @@
+const axios = require('axios');
+
+const canRequest = (url) => {
+  const [, , host] = url.split('/')
+
+  if (
+    ![
+      'oapi.dingtalk.com',
+      'cdn.jsdelivr.net',
+      'lkyl.dianpusoft.cn',
+      'raw.githubusercontent.com',
+    ].includes(host) &&
+    !/\.jd\.com$/.test(host) &&
+    !/\.jingxi\.com/.test(host) &&
+    !/\.isvjcloud\.com/.test(host)
+  ) {
+    console.log(host, url)
+    throw new Error(`该请求 url 不合法: ${url}`)
+  }
+}
+
+axios.interceptors.request.use((options) => {
+  canRequest(options.url)
+
+  return options
+})
+
+module.exports = axios
